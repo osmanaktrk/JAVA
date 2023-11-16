951234f104403dbbdb544fd4a1b7271a3651f6a4
Osman <74795147+osmanaktrk@users.noreply.github.com>
2023-11-16 18:51:50 +0100

    Update Oefening1.java
    Ik heb iets verandert met mijn programma. Ik heb een array gebruikt om het resultaat van berekenTotaalFruitBak() functie te kunnen terugsturen naar main programma.

--- Oefening1.java
+++ Oefening1.java
@@ -1,16 +1,11 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Oefening1 {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int grootfruitbak = 0;
         double totaalPrijs=0;
         double totaalPrijsBTW = 0;
-
-
-
 
         do {
             System.out.print("Hoeveel stukken wil je kopen (5-20)?: ");
@@ -22,89 +17,109 @@ public class Oefening1 {
 
         String [] keuzes = new String[grootfruitbak];
 
+        vulFruitbak(grootfruitbak,keuzes);
 
-        System.out.println("Keuzes: 'Grone Appel', 'Rode Appel', 'Peer'");
-        System.out.println("Om grone appel te kiezen geef 'GA' in");
-        System.out.println("Om rode appel te kiezen 'RA' in");
-        System.out.println("Om peer te kiezen geef 'P' in");
-        totaalPrijs = vulFruitBak(keuzes, totaalPrijs, grootfruitbak, sc);
-        totaalPrijsBTW = berekenenTotaalFruitBakBTW(totaalPrijs,totaalPrijsBTW);
-        System.out.println("De waarde van de mand in: "+totaalPrijs+" Euro");
+        totaalPrijs = berekenenTotaalFruitBak(keuzes);
+        System.out.println("De waarde van je mand is: "+totaalPrijs+" Euro");
+        totaalPrijsBTW = berekenenTotaalFruitBakBtw(totaalPrijs);
         System.out.println("Inclusief met btw is dit: "+totaalPrijsBTW+" Euro");
 
 
 
 
+
+
+
     }
 
-    public static double vulFruitBak(String [] keuzes, double totaalPrijs, int groodfruidbak , Scanner sc){
+
+    public static void vulFruitbak(int grootfruitbak, String[] keuzes){
+        Scanner sc = new Scanner(System.in);
         String keuze;
-        int teller = 0;
-        do {
-            System.out.print("Welke stukken fruit pluk je? (keuze: GA, RA, P): ");
-            keuze = sc.nextLine();
 
-            if (!keuze.equals("GA") && !keuze.equals("RA") && !keuze.equals("P")) {
-                System.out.println("Foutive keuze, probier opnieuw");
 
-            } else {
-                keuzes[teller] = keuze;
-                teller++;
-                totaalPrijs =berekenentotaalfruitbak(keuze, totaalPrijs);
+        System.out.println("Keuzes: 'Grone Appel', 'Rode Appel', 'Peer'");
+        System.out.println("Om grone appel te kiezen geef 'GA' in");
+        System.out.println("Om rode appel te kiezen 'RA' in");
+        System.out.println("Om peer te kiezen geef 'P' in");
+
+
+        for (int i = 0; i < grootfruitbak; i++){
+
+
+            do {
+                System.out.println("Welke stukken fruit pluk je? (keuze: GA, RA, P)");
+                System.out.print("Keuze"+(i+1)+") ");
+                keuze = sc.nextLine();
+
+                if (!keuze.equals("GA") && !keuze.equals("RA") && !keuze.equals("P")) {
+                    System.out.println("Foutive keuze, probier opnieuw");
+                }
+
+            } while (!keuze.equals("GA") && !keuze.equals("RA") && !keuze.equals("P"));
+
+            keuzes[i] = keuze;
+
+        }
+
+
+
+
+    }
+
+    public static double berekenenTotaalFruitBak(String [] keuzes){
+
+        double rodeappel = 0;
+        double groeneappel = 0;
+        double peer = 0;
+        double totaal = 0;
+
+
+
+        for (int i = 0; i<keuzes.length; i++){
+
+            switch (keuzes[i]) {
+
+
+                case "GA":
+                    groeneappel++;
+
+                    break;
+
+
+                case "RA":
+
+                    rodeappel++;
+
+                    break;
+
+
+                case "P":
+
+                    peer++;
+
+                    break;
 
             }
-        }while (teller!=groodfruidbak);
-
-
-
-        return totaalPrijs;
-
-
-    }
-
-    public static double berekenentotaalfruitbak(String keuze, double totaalPrijs){
-
-        double prijsRodeAppel = 1;
-        double prijsGroneAppel = 0.5;
-        double prijsPeer = 1.5;
-
-        switch (keuze){
-
-            case "GA":
-                totaalPrijs+=prijsGroneAppel;
-
-                break;
-
-
-            case "RA":
-                totaalPrijs+=prijsRodeAppel;
-
-                break;
-
-
-
-            case "P":
-                totaalPrijs+=prijsPeer;
-                break;
-
-
 
 
 
         }
 
+        totaal += (rodeappel*(1))+(groeneappel*(0.5))+(peer*(1.5));
 
-        return totaalPrijs;
+
+
+
+        return totaal;
     }
 
-    public static double berekenenTotaalFruitBakBTW(double totaalPrijs, double totaalPrijsBTW){
+    public static double berekenenTotaalFruitBakBtw(double totaalPrijs){
 
-        totaalPrijsBTW =totaalPrijs+ ((totaalPrijs/100)*21);
+        double totaalbtw = totaalPrijs+(totaalPrijs/100)*21;
 
-
-        return totaalPrijsBTW;
+        return totaalbtw;
     }
-
 
 
 
